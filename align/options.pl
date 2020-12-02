f0fed53a74a67291e724473745a5fba676a662d8
Thomas E Wilson <wilsonte@wilsonte-n1.mbni.org>
2020-12-01 20:54:40 -0500

    tweak align options

--- align/options.pl
+++ align/options.pl
@@ -11,6 +11,30 @@ push @{$options{align}}, (
         required    => 1,
         default     => undef,
         message     => "every sub-directory of input-dir will be searched for cell FASTQ files",
+    },
+    {           
+        long        => 'genome',
+        short       => 'g',
+        type        => 'str',            
+        required    => 1,
+        default     => undef,
+        message     => "the reference genome to which reads were aligned",
+    },
+    {           
+        long        => 'genome-dir',
+        short       => 'G',
+        type        => 'str',            
+        required    => 1,
+        default     => undef,
+        message     => "the directory in which reference genome files can be found",
     },    
+    {           
+        long        => 'min-mapq',
+        short       => 'Q',
+        type        => 'int',            
+        required    => 0,
+        default     => 5,
+        message     => "reject read alignments with lower than this MAPQ",
+    },
 );
 
